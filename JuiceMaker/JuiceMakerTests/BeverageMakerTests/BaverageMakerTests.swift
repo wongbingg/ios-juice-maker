1aec14d1eaad3bdbcbf6f8896519785ebd5b3ec0
wongbingg <wb277@naver.com>
2022-10-18 22:00:16 +0900

    test : 테스트코드 수정

--- JuiceMaker/JuiceMakerTests/BeverageMakerTests/BaverageMakerTests.swift
+++ JuiceMaker/JuiceMakerTests/BeverageMakerTests/BaverageMakerTests.swift
@@ -28,16 +28,18 @@ final class BeverageMakerTests: XCTestCase {
     func test_딸기쥬스를만들때_딸기재고가부족하다는오류가뜨는지() {
         // given
         // when
+        baverageMaker.make(baverage: .strawberryJuice)
         // then
-        XCTAssertThrowsError(try baverageMaker.make(baverage: .strawberryJuice))
+        
+        
     }
     
     func test_바나나쥬스를만들고_재고가8개남았는지() {
         // given
         let expectation = 8
         // when
-        try! baverageMaker.make(baverage: .bananaJuice)
-        let result = baverageMaker.fruitStore.fetchCount(of: .banana)
+        baverageMaker.make(baverage: .bananaJuice)
+        let result = fruitStore.fetchCount(of: .banana)
         // then
         XCTAssertEqual(expectation, result)
     }
